80da432933e47c7abef5e08d8ee21c67a10afbd5
Omar Betanzos <dashdancin@gmail.com>
2023-11-06 16:56:49 -0600

    COM ServiceWorker and COM Manifest Json

--- src/index.js
+++ src/index.js
@@ -6,10 +6,23 @@ import "./styles/abcdemedia.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+// Verificar si el navegador admite Service Workers
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js") // Ruta al archivo service-worker.js
+      .then((registration) => {
+        console.log("Service Worker registrado con éxito:", registration);
+      })
+      .catch((error) => {
+        console.log("Error al registrar el Service Worker:", error);
+      });
+  });
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Si deseas medir el rendimiento de tu aplicación, puedes utilizar reportWebVitals.
 reportWebVitals();
+
