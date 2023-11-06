5f48b154ef4e8cdf1625d91a17cf419e64d99b64
Omar Betanzos <dashdancin@gmail.com>
2023-11-06 17:17:02 -0600

    Relocate SW

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open('my-cache').then(cache => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/css/styles.css',
+          '/js/app.js'
+          // Agrega aquí más recursos que deseas cachear
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
