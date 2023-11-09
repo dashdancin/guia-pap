e998c9201994b2a11fe7925728a45d2e00fa75f1
dashdancin <dashdancin@gmail.com>
2023-11-09 15:34:37 -0600

    Merge index.js

--- src/index.js
+++ src/index.js
@@ -19,28 +19,6 @@ if ("serviceWorker" in navigator) {
       });
   });
 }
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
-
-<<<<<<< HEAD
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
-        .then(registration => {
-          console.log('Service Worker registrado con éxito:', registration);
-        })
-        .catch(error => {
-          console.log('Error al registrar el Service Worker:', error);
-        });
-    });
-  }
   
-=======
-// Si deseas medir el rendimiento de tu aplicación, puedes utilizar reportWebVitals.
->>>>>>> b7dab3488934a6d195e3dabb25d5382057ab762a
 reportWebVitals();
 
