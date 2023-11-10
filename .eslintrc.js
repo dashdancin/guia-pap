454ac3ab4d1ada0f7fdb2371a143abc576f48662
dashdancin <dashdancin@gmail.com>
2023-11-09 18:15:13 -0600

    NEW eslin config file

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // ... Otras configuraciones de ESLint ...
+    rules: {
+      // ... Otras reglas ...
+      "no-restricted-globals": ["error", "self"], // Permitir el uso de "self"
+    },
+  };
+  
