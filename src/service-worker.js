b69c655ba994dcaacdba1d94bf751b404f3dcdbe
dashdancin <dashdancin@gmail.com>
2023-11-14 12:48:28 -0600

    FIX self.__WB_MANIFEST issue

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,49 +1,19 @@
 // src/service-worker.js
 
+/* eslint-disable no-restricted-globals */
+
+// Importar el manifiesto precache generado por Workbox 
+
+importScripts('precache-manifest.{hash}.js');
+
 const CACHE_VERSION = 'my-cache-v2';
 
-self.addEventListener('install', event => {
-  /* eslint-disable no-restricted-globals */
-  event.waitUntil(
-    caches.open(CACHE_VERSION)
-      .then(cache => {
-        return cache.addAll([
-          '/',
-          '/index.css',
-          '/index.js',
-        ]);
-      })
-      .then(() => {
-        return self.skipWaiting();
-      })
-  );
-  /* eslint-enable no-restricted-globals */
-});
+self.addEventListener('install', event => { event.waitUntil( caches.open(CACHE_VERSION) .then(cache => { return cache.addAll(precacheManifest); }) .then(() => { return self.skipWaiting(); }) ); });
 
-self.addEventListener('activate', event => {
-  /* eslint-disable no-restricted-globals */
-  event.waitUntil(
-    caches.keys()
-      .then(cacheNames => {
-        return Promise.all(
-          cacheNames.map(cacheName => {
-            if (cacheName !== CACHE_VERSION) {
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      })
-  );
-  /* eslint-enable no-restricted-globals */
-});
+self.addEventListener('activate', event => { event.waitUntil( caches.keys() .then(cacheNames => { return Promise.all( cacheNames.map(cacheName => { if (cacheName !== CACHE_VERSION) { return caches.delete(cacheName); } }) ); }) .then(() => self.clients.claim()) ); });
 
-self.addEventListener('fetch', event => {
-  /* eslint-disable no-restricted-globals */
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
-  /* eslint-enable no-restricted-globals */
-});
+self.addEventListener('fetch', event => { event.respondWith( caches.match(event.request) .then(response => { return response || fetch(event.request); }) ); });
+
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+/* eslint-enable no-restricted-globals */
