c5d22c3f2ec4520a044d40e3b774c3f49773b76f
dashdancin <dashdancin@gmail.com>
2023-11-23 11:23:30 -0600

    ADD notification for install

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import Cover from "./components/Cover";
 import ConceptDefinition from "./components/ConceptDefinition";
 import Use from "./components/Use";
@@ -14,52 +15,138 @@ import SupportApp from "./components/SupportApp";
 import SupportLinks from "./components/SupportLinks";
 import Info from "./components/Info";
 
+// function App() {
+//  return (
+//   <>
+//    <div className="fragment">
+//     <div className="cover">
+//      <Cover />
+//     </div>
+//     <div className="concept">
+//      <ConceptDefinition />
+//     </div>
+//     <div className="use">
+//      <Use />
+//     </div>
+//     <div className="profits">
+//      <Profits />
+//     </div>
+//     <div className="a">
+//      <A />
+//     </div>
+//     <div className="b">
+//      <B />
+//     </div>
+//     <div className="c">
+//      <C />
+//     </div>
+//     <div className="d">
+//      <D />
+//     </div>
+//     <div className="e">
+//      <E />
+//     </div>
+//     <div>
+//      <Consideration />
+//     </div>
+//     <div>
+//      <Do />
+//      <Dont />
+//     </div>
+//     <div>
+//      <SupportApp />
+//      <SupportLinks />
+//      <Info />
+//     </div>
+//    </div>
+//   </>
+//  );
+// }
+
+// export default App;
+
+// src/App.js
+
 function App() {
- return (
-  <>
-   <div className="fragment">
-    <div className="cover">
-     <Cover />
+  useEffect(() => {
+    let deferredPrompt;
+
+    const showInstallPrompt = () => {
+      const installPrompt = window.confirm('¡Instala nuestra aplicación para una mejor experiencia!');
+
+      if (installPrompt) {
+        deferredPrompt.prompt();
+
+        deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('Usuario aceptó la instalación');
+          } else {
+            console.log('Usuario rechazó la instalación');
+          }
+
+          deferredPrompt = null;
+        });
+      } else {
+        console.log('Usuario eligió "Más tarde"');
+      }
+    };
+
+    window.addEventListener('beforeinstallprompt', (event) => {
+      event.preventDefault();
+      deferredPrompt = event;
+      showInstallPrompt();
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      {/* Contenido de tu aplicación React */
+        <>
+        <div className="fragment">
+         <div className="cover">
+          <Cover />
+         </div>
+         <div className="concept">
+          <ConceptDefinition />
+         </div>
+         <div className="use">
+          <Use />
+         </div>
+         <div className="profits">
+          <Profits />
+         </div>
+         <div className="a">
+          <A />
+         </div>
+         <div className="b">
+          <B />
+         </div>
+         <div className="c">
+          <C />
+         </div>
+         <div className="d">
+          <D />
+         </div>
+         <div className="e">
+          <E />
+         </div>
+         <div>
+          <Consideration />
+         </div>
+         <div>
+          <Do />
+          <Dont />
+         </div>
+         <div>
+          <SupportApp />
+          <SupportLinks />
+          <Info />
+         </div>
+        </div>
+       </>
+       }
     </div>
-    <div className="concept">
-     <ConceptDefinition />
-    </div>
-    <div className="use">
-     <Use />
-    </div>
-    <div className="profits">
-     <Profits />
-    </div>
-    <div className="a">
-     <A />
-    </div>
-    <div className="b">
-     <B />
-    </div>
-    <div className="c">
-     <C />
-    </div>
-    <div className="d">
-     <D />
-    </div>
-    <div className="e">
-     <E />
-    </div>
-    <div>
-     <Consideration />
-    </div>
-    <div>
-     <Do />
-     <Dont />
-    </div>
-    <div>
-     <SupportApp />
-     <SupportLinks />
-     <Info />
-    </div>
-   </div>
-  </>
- );
+  );
 }
 
 export default App;
